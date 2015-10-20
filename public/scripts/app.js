26a319136cd49f643fa9c3831f922dea88931e51
geir <geir@tutanota.com>
2015-10-20 21:20:49 +0200

    web part completely rewritten

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,8 +3,10 @@ app = angular.module('app', ['ngRoute']);
 app.config(function($routeProvider, $locationProvider){
   $routeProvider
     .when('/', {templateUrl: '/partials/main.html'})
-    .when('/map', {templateUrl: '/partials/map.html',
-                   controller: 'MapCtrl'})
+    .when('/login', {templateUrl: '/partials/login.html',
+                   controller: 'AuthenticationController'})
+    .when('/signup', {templateUrl: '/partials/signup.html',
+                   controller: 'AuthenticationController'})
     .otherwise({redirectTo: '/'});
 
   $locationProvider.html5Mode(true);
